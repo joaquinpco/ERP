642f5bac3fb11f4a15e50c67635e733ae73202e0
joaquinpco <joaquin1208@me.com>
2020-11-14 21:54:14 +0100

    Profile and edit UI frontend  updated

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { API, Auth } from 'aws-amplify';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  public user: any;
+
+  constructor(
+    private actionSheetController: ActionSheetController  
+  ) {
+    this.user = new Object();
+    this.user.normalizeAttr = [];
+  }
 
   ngOnInit() {
+  }
+
+  async openActionSheet()
+  {
+    await this.presentActionSheet();
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Profile',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Edit First and Last Name',
+        icon: 'document-text-outline',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Change profile image from Camera',
+        icon: 'camera-outline',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Change profile image from Album',
+        icon: 'image-outline',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  async ionViewWillEnter()
+  {
+    try
+    {
+      const currentUser = await Auth.currentAuthenticatedUser();
+      let params = {
+        'queryStringParameters' :
+        {
+          'Username' : currentUser.username
+        }
+      };
+      this.user = await API.get('ERP', '/erp/getNormalizeUser', params)
+    }
+    catch(err)
+    {
+      this.user = undefined;
+    }
   }
 
 }
